d0febb2e076644fa1592d9f8fb4c79373ac838e0
Manuel Keilman <e11824531@student.tuwien.ac.at>
2022-10-26 11:34:29 +0200

    initial commit

---
+++ src/main/java/com/example/twelve_factor_microservice/TwelveFactorMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.twelve_factor_microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwelveFactorMicroserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TwelveFactorMicroserviceApplication.class, args);
+    }
+
+}
